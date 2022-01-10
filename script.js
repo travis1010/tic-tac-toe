600e2a8b2e84fdc59c9c22d9d439263d5f3c0ba7
Travis Hatley <travis1010@gmail.com>
2022-01-09 22:19:13 -0700

    added line to show win

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 const gameBoard = (() => {
-  const grid = [['X', 'X', ''],
-                ['', 'O', ''],
-                ['X', 'O', '']];
-
+  const grid = [['', '', ''],
+                ['', '', ''],
+                ['', '', '']];
 
   const printGrid = () => {
     clearGrid();
@@ -30,24 +29,37 @@ const gameBoard = (() => {
       })
     })
   };
+
   const updateTile = (tileDiv, char) => {
     addCharToGrid(char, tileDiv.getAttribute('data-y'), tileDiv.getAttribute('data-x'))
     printGrid();
     console.log(isGameOver());
   };
+
   const clearGrid = () => {
     const gameGrid = document.getElementById('game-board');
     while (gameGrid.firstChild) {
       gameGrid.removeChild(gameGrid.firstChild)
     }
   };
+
   const addCharToGrid = (char, row, column) => {
     grid[row][column] = char;
   };
+
   const isGameOver = () => {
     //check rows
     for (let i = 0; i < 3; i++) {
-      if (grid[i].every(tile => tile == 'X') || grid[i].every(tile => tile == 'O')) return true;
+      if (grid[i].every(tile => tile == 'X') || grid[i].every(tile => tile == 'O')) {
+        if (i == 0) {
+          adjustVictoryLine('-100px');
+        } else if (i == 2) {
+          adjustVictoryLine('100px');
+        }
+        
+        showVictoryLine();
+        return true;
+      }
     }
     //check columns 
     for (let i = 0; i < 3; i++) {
@@ -55,12 +67,40 @@ const gameBoard = (() => {
       for (let j = 0; j < 3; j++) {
         column.push(grid[j][i]);
       }
-      if (column.every(tile => tile == 'X') || column.every(tile => tile == 'O')) return true;
+      if (column.every(tile => tile == 'X') || column.every(tile => tile == 'O')) {
+        if (i == 0) {
+          adjustVictoryLine('-100px', '-90deg');
+        } else if (i == 1) {
+          adjustVictoryLine('0px', '-90deg');
+        } else if (i == 2) {
+          adjustVictoryLine('100px', '-90deg');
+        }
+        showVictoryLine();
+        return true;
+      }
+      
     }
     //check diagonals
-    if ((grid[0][0] == grid[1][1]) && (grid[1][1] == grid[2][2]) && (grid[1][1] != '')) return true;
-    if ((grid[0][2] == grid[1][1]) && (grid[1][1] == grid[2][0]) && (grid[1][1] != '')) return true;
+    if ((grid[0][0] == grid[1][1]) && (grid[1][1] == grid[2][2]) && (grid[1][1] != '')) {
+      adjustVictoryLine('0px', '45deg', '1.3');
+      showVictoryLine();
+      return true;
+    }
+    if ((grid[0][2] == grid[1][1]) && (grid[1][1] == grid[2][0]) && (grid[1][1] != '')){ 
+      adjustVictoryLine('0px', '-45deg', '1.3');
+      showVictoryLine();
+      return true;
+    }
     return false;
+  }
+
+  const showVictoryLine = () => {
+    document.getElementById('victory-line').style.display = 'flex';
+  }
+
+  const adjustVictoryLine = (moveY, rotate='0deg', length='1') => {
+    let victoryLine = document.getElementById('victory-line');
+    victoryLine.style.transform = `rotate(${rotate}) translateY(${moveY}) scaleX(${length})`;
   }
   return { 
     printGrid,
